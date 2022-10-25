3c728b69124b04e9f0253cdee6a54df388a3c3e7
Diego Geringer <geringerdiego@hotmail.com>
2022-10-25 15:20:02 -0300

    48

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -2,7 +2,7 @@ import { initialFunction } from '../src/prueba/index'
 
 test('Test Example', () => {
     const test = initialFunction();
-    expect(test).toBe('Library');
+    expect(test).toBe('Library1');
 })
 
 

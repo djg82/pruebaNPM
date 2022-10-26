8ec01db6f05596ab6cb36c7f7d3b561a1ac7ddeb
Diego Geringer <geringerdiego@hotmail.com>
2022-10-26 17:59:47 -0300

    145

--- src/salidas.ts
+++ src/salidas.ts
@@ -1,3 +1,4 @@
 export { initialFunction } from './prueba/index'
 export { initialFunction2 } from './prueba2/index'
 export { initialFunction3 } from './prueba3/index'
+export { initialFunction4 } from './prueba4/index'
--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,12 +1,21 @@
-import { initialFunction } from '../src/prueba/index'
-import { initialFunction2 } from '../src/prueba2/index'
+import { initialFunction, initialFunction2, initialFunction3, initialFunction4 } from '../src/salidas'
 
 test('Test Example', () => {
     const test = initialFunction();
     expect(test).toBe('Library');
 })
 
-test('Test Example', () => {
+test('Test Example2', () => {
     const test = initialFunction2();
     expect(test).toBe('Library2');
+})
+
+test('Test Example3', () => {
+    const test = initialFunction3();
+    expect(test).toBe('Library3');
+})
+
+test('Test Example4', () => {
+    const test = initialFunction4();
+    expect(test).toBe('LibraryCuatro');
 })
